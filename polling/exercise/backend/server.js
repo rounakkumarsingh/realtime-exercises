9e22249e303f1147dc794522070d61965acd9eeb
Rounak Kumar Singh <rounak.official.2@gmail.com>
2025-01-27 20:14:55 +0530

    refactor: improve code formatting and structure in biome configuration and server logic

--- polling/exercise/backend/server.js
+++ polling/exercise/backend/server.js
@@ -1,7 +1,7 @@
-import express from "express";
 import bodyParser from "body-parser";
-import nanobuffer from "nanobuffer";
+import express from "express";
 import morgan from "morgan";
+import nanobuffer from "nanobuffer";
 
 // set up a limited array
 const msg = new nanobuffer(50);
@@ -9,9 +9,9 @@ const getMsgs = () => Array.from(msg).reverse();
 
 // feel free to take out, this just seeds the server with at least one message
 msg.push({
-  user: "brian",
-  text: "hi",
-  time: Date.now(),
+    user: "brian",
+    text: "hi",
+    time: Date.now(),
 });
 
 // get express ready to run
@@ -20,14 +20,25 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(express.static("frontend"));
 
-app.get("/poll", function (req, res) {
-  // use getMsgs to get messages to send back
-  // write code here
+app.get("/poll", (_, res) => {
+    // use getMsgs to get messages to send back
+    // write code here
+    return res.json({ msgs: getMsgs() });
 });
 
-app.post("/poll", function (req, res) {
-  // add a new message to the server
-  // write code here
+app.post("/poll", (req, res) => {
+    // add a new message to the server
+    // write code here
+
+    const { user, text } = req.body;
+
+    msg.push({
+        user,
+        text,
+        time: Date.now(),
+    });
+
+    res.json({ status: "ok" });
 });
 
 // start the server
--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -8,34 +8,63 @@ let allChat = [];
 const INTERVAL = 3000;
 
 // a submit listener on the form in the HTML
-chat.addEventListener("submit", function (e) {
-  e.preventDefault();
-  postNewMsg(chat.elements.user.value, chat.elements.text.value);
-  chat.elements.text.value = "";
+chat.addEventListener("submit", (e) => {
+    e.preventDefault();
+    postNewMsg(chat.elements.user.value, chat.elements.text.value);
+    chat.elements.text.value = "";
 });
 
 async function postNewMsg(user, text) {
-  // post to /poll a new message
-  // write code here
+    // post to /poll a new message
+    // write code here
+    const data = {
+        user,
+        text,
+    };
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    try {
+        const res = await fetch("/poll", options);
+        const json = await res.json();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 async function getNewMsgs() {
-  // poll the server
-  // write code here
+    // poll the server
+    // write code here
+    let json;
+    try {
+        const res = await fetch("/poll");
+        json = await res.json();
+    } catch (error) {
+        console.error(error);
+    }
+
+    allChat = json.msgs;
+    render();
+    setTimeout(getNewMsgs, INTERVAL);
 }
 
 function render() {
-  // as long as allChat is holding all current messages, this will render them
-  // into the ui. yes, it's inefficent. yes, it's fine for this example
-  const html = allChat.map(({ user, text, time, id }) =>
-    template(user, text, time, id)
-  );
-  msgs.innerHTML = html.join("\n");
+    // as long as allChat is holding all current messages, this will render them
+    // into the ui. yes, it's inefficent. yes, it's fine for this example
+    const html = allChat.map(({ user, text, time, id }) =>
+        template(user, text, time, id)
+    );
+    msgs.innerHTML = html.join("\n");
 }
 
 // given a user and a msg, it returns an HTML string to render to the UI
 const template = (user, msg) =>
-  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
+    `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
 
 // make the first request
 getNewMsgs();
