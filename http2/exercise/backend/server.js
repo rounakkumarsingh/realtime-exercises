4635f2e37cadbd999f1e1dc5c5b76862e9347f7e
Rounak Kumar Singh <rounak.official.2@gmail.com>
2025-01-27 20:51:49 +0530

    refactor: update import statements to use node: prefix and improve code formatting

--- http2/exercise/backend/server.js
+++ http2/exercise/backend/server.js
@@ -1,19 +1,19 @@
-import http2 from "http2";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-import handler from "serve-handler";
+import fs from "node:fs";
+import http2 from "node:http2";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import nanobuffer from "nanobuffer";
+import handler from "serve-handler";
 
-let connections = [];
+const connections = [];
 
 const msg = new nanobuffer(50);
 const getMsgs = () => Array.from(msg).reverse();
 
 msg.push({
-  user: "brian",
-  text: "hi",
-  time: Date.now(),
+    user: "brian",
+    text: "hi",
+    time: Date.now(),
 });
 
 // the two commands you'll have to run in the root directory of the project are
@@ -25,8 +25,8 @@ msg.push({
 // http2 only works over HTTPS
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const server = http2.createSecureServer({
-  cert: fs.readFileSync(path.join(__dirname, "/../server.crt")),
-  key: fs.readFileSync(path.join(__dirname, "/../key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "/../server.crt")),
+    key: fs.readFileSync(path.join(__dirname, "/../key.pem")),
 });
 
 /*
@@ -36,35 +36,36 @@ const server = http2.createSecureServer({
  */
 
 server.on("request", async (req, res) => {
-  const path = req.headers[":path"];
-  const method = req.headers[":method"];
+    const path = req.headers[":path"];
+    const method = req.headers[":method"];
 
-  if (path !== "/msgs") {
-    // handle the static assets
-    return handler(req, res, {
-      public: "./frontend",
-    });
-  } else if (method === "POST") {
-    // get data out of post
-    const buffers = [];
-    for await (const chunk of req) {
-      buffers.push(chunk);
+    if (path !== "/msgs") {
+        // handle the static assets
+        return handler(req, res, {
+            public: "./frontend",
+        });
     }
-    const data = Buffer.concat(buffers).toString();
-    const { user, text } = JSON.parse(data);
+    if (method === "POST") {
+        // get data out of post
+        const buffers = [];
+        for await (const chunk of req) {
+            buffers.push(chunk);
+        }
+        const data = Buffer.concat(buffers).toString();
+        JSON.parse(data);
 
-    /*
-     *
-     * some code goes here
-     *
-     */
-  }
+        /*
+         *
+         * some code goes here
+         *
+         */
+    }
 });
 
 // start listening
 const port = process.env.PORT || 8080;
 server.listen(port, () =>
-  console.log(
-    `Server running at https://localhost:${port} - make sure you're on httpS, not http`
-  )
+    console.log(
+        `Server running at https://localhost:${port} - make sure you're on httpS, not http`
+    )
 );
