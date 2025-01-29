f50e44bd1e1fe2b56ff64a350db8786ab8335c9e
Rounak Kumar Singh <rounak.official.2@gmail.com>
2025-01-30 02:06:29 +0530

    feat: implement socket.io for real-time messaging and update server handling

--- websockets/exercise-socketio/backend/server.js
+++ websockets/exercise-socketio/backend/server.js
@@ -1,22 +1,22 @@
-import http from "http";
-import handler from "serve-handler";
+import http from "node:http";
 import nanobuffer from "nanobuffer";
+import handler from "serve-handler";
 import { Server } from "socket.io";
 
 const msg = new nanobuffer(50);
 const getMsgs = () => Array.from(msg).reverse();
 
 msg.push({
-  user: "brian",
-  text: "hi",
-  time: Date.now(),
+    user: "brian",
+    text: "hi",
+    time: Date.now(),
 });
 
 // serve static assets
 const server = http.createServer((request, response) => {
-  return handler(request, response, {
-    public: "./frontend",
-  });
+    return handler(request, response, {
+        public: "./frontend",
+    });
 });
 
 /*
@@ -25,7 +25,26 @@ const server = http.createServer((request, response) => {
  *
  */
 
+const io = new Server(server, {});
+io.on("connection", (socket) => {
+    console.log("Connected");
+
+    socket.emit("msg:get", { msg: getMsgs() });
+
+    socket.on("msg:emit", (data) => {
+        msg.push({
+            ...data,
+            time: Date.now(),
+        });
+        io.emit("msg:get", { msg: getMsgs() });
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Disconnected");
+    });
+});
+
 const port = process.env.PORT || 8080;
 server.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}`)
+    console.log(`Server running at http://localhost:${port}`)
 );
--- websockets/exercise-socketio/frontend/socketio-chat.js
+++ websockets/exercise-socketio/frontend/socketio-chat.js
@@ -11,24 +11,37 @@ let allChat = [];
  *
  */
 
-chat.addEventListener("submit", function (e) {
-  e.preventDefault();
-  postNewMsg(chat.elements.user.value, chat.elements.text.value);
-  chat.elements.text.value = "";
+const socket = io("http://localhost:8080/");
+
+socket.on("connect", () => {
+    console.log("Socket connection successfull");
+    presence.innerText = "🍏";
 });
 
-async function postNewMsg(user, text) {
-  /*
-   *
-   * Code goes here
-   *
-   */
-}
+socket.on("disconnect", () => {
+    presence.innerText = "⭕";
+});
+
+socket.on("msg:get", (data) => {
+    allChat = data.msg;
+    render();
+});
+
+chat.addEventListener("submit", (e) => {
+    e.preventDefault();
+    postNewMsg(chat.elements.user.value, chat.elements.text.value);
+    chat.elements.text.value = "";
+});
+
+const postNewMsg = async (user, text) => {
+    const data = { user, text };
+    socket.emit("msg:emit", data);
+};
 
 function render() {
-  const html = allChat.map(({ user, text }) => template(user, text));
-  msgs.innerHTML = html.join("\n");
+    const html = allChat.map(({ user, text }) => template(user, text));
+    msgs.innerHTML = html.join("\n");
 }
 
 const template = (user, msg) =>
-  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
+    `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
